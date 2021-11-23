6eddc76328698127a1410bcf0a2565bb34646029
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-11-23 10:18:25 +0000

    added and styled form

--- munchies/src/pages/create/Create.js
+++ munchies/src/pages/create/Create.js
@@ -1,9 +1,54 @@
+import React, { useState } from "react";
 import "./Create.css";
 
-import React from "react";
-
 const Create = () => {
-  return <div>Create</div>;
+  const [method, setMethod] = useState("");
+  const [title, setTitle] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add Yum Yum</h2>
+
+      <form onSubmit={handleSumbit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking Time(mins)</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="create-button">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
